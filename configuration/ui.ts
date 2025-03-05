ec130bdcaa09ac9c2d0fb80fe80f70899a611d32
Arya Salian <138736627+aryasalian@users.noreply.github.com>
2025-03-05 02:29:01 -0500

    Updated UI

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Mr. Leonardo DiCapital`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI by Arya Salian`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const FOOTER_MESSAGE: string = `Quantitative Finance AI Expert by Arya Salian`;
+export const CLEAR_BUTTON_TEXT: string = `Clear my chat history!`;
+export const PAGE_TITLE: string = `Leonardo DiCapital | Arya's Personal Quant`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant and personal quant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
