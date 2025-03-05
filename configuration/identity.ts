2b38e51fd3a07b7af19cc3f21a9ffaae11be45cd
Arya Salian <138736627+aryasalian@users.noreply.github.com>
2025-03-05 01:51:14 -0500

    Update owner and ai names

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
+export const OWNER_NAME: string = `Arya`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Global Trade Navigator`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
