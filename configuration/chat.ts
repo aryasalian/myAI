f1d5a92b7772080d734020ad54f19d495a793dfe
Arya Salian <138736627+aryasalian@users.noreply.github.com>
2025-03-05 02:45:57 -0500

    Updated msg styles to match leonardo

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
-export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant and personal quant. Nice to meet you!`;
+export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I can’t seem to find the right direction on this one, just like a ship that’s hit an iceberg haha. Please try again in a bit.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `Woah, woah, woah...information overload! Slow down there, I'm just an AI :(\n Please reduce the word count of your prompt.`;
+export const WORD_BREAK_MESSAGE: string = `Whoa, I may be the Wolf of Wall Street but this is too much information all at once. Let's slow it down a bit, give me less words.`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
