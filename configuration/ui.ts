239ca34e5c32cc2fbbee932157f60353ef0d4f81
Arya Salian <138736627+aryasalian@users.noreply.github.com>
2025-03-05 02:49:43 -0500

    Updated msg placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Mr. Leonardo DiCapital`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Hold tight—this Wolf is fetching your answer straight from Wall Street. Just a moment, please.`;
 export const FOOTER_MESSAGE: string = `Quantitative Finance AI Expert by Arya Salian`;
 export const CLEAR_BUTTON_TEXT: string = `Clear my chat history!`;
 export const PAGE_TITLE: string = `Leonardo DiCapital | Arya's Personal Quant`;
